a799a0e8bc7b15be3a3c4af8c027581287f76002
yonykikok <jonathan.n.haedo@gmail.com>
2021-03-24 15:51:22 -0300

    clase creada

--- src/app/clases/usuario.ts
+++ src/app/clases/usuario.ts
@@ -1,4 +1,23 @@
 export class Usuario {
+    nombreUsuario;   
+    apellidoUsuario;   
+    DNIUsuario;  
+    correoUsuario;   
+    tipoUsuario;  
+    fotoUsuario;
 
-    
+    constructor( nombreUsuario,   
+        apellidoUsuario,   
+        DNIUsuario,  
+        correoUsuario,   
+        tipoUsuario,  
+        fotoUsuario){
+            this.nombreUsuario=nombreUsuario;
+            this.apellidoUsuario=apellidoUsuario;
+            this.DNIUsuario=DNIUsuario;
+            this.correoUsuario=correoUsuario;
+            this.tipoUsuario=tipoUsuario;
+            this.fotoUsuario=fotoUsuario;
+    }
+  
 }
