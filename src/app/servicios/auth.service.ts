2b4481d986458cf9fd860f1ea1ddc01a3ac882ab
Lucho <lucho.moreno@live.com>
2021-04-02 15:54:17 -0300

    Solucionar todo

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from "@angular/fire/auth";
+
 import { Usuario } from '../clases/usuario';
+
+// FIRESTORE
+import {AngularFirestore} from "@angular/fire/firestore";
+
+import jwt_decode from "jwt-decode"; // ESTO LO OBTENGO CON npm i jwt-decode
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
   public isLogged:any =false;
-  public user:Usuario;
-  constructor(private AFauth : AngularFireAuth) { }
+
+  public user : Usuario;
+
+  tokenUsuario;
+  payloadUsuario;
+  emailUsuario;
+
+  constructor(private AFauth : AngularFireAuth, private firestore : AngularFirestore) { }
+
 
 
   // FUNCION PARA LOGUEARSE EN EL SISTEMA.
@@ -53,6 +67,29 @@ register(email : string, password : string){
   });
 
 }
+
+
+buscarUsuarioLogueado() 
+{
+  this.tokenUsuario = localStorage.getItem('token');
+  this.payloadUsuario = jwt_decode(this.tokenUsuario);
+  this.emailUsuario = this.payloadUsuario.email;
+
+  let usuario:Usuario;
+
+    this.firestore.collection('usuarios').get().subscribe((querySnapShot) => {
+      querySnapShot.forEach((doc) => {
+      if(doc.data()['correo'].toUpperCase() == this.emailUsuario.toUpperCase())
+       { 
+         usuario= new Usuario(doc.data()['nombre'],doc.data()['apellido'],doc.data()['DNI'],doc.data()['correo'],doc.data()['tipo'],doc.data()['foto']);
+         this.user = usuario;
+         console.log("ENCUENTRO EL USUARIO")
+       }
+      })
+    })
+}
+
+
 
 
 }
