25d73436a1324aa5b114f11ea592342e86c6b2cb
Lucho <lucho.moreno@live.com>
2021-04-06 04:31:35 -0300

    Se modificó el diseño de consultas en cards, por un diseño de tablas. Falta agregar responsive

--- src/app/componentesEmpleado/responder-consultas/responder-consultas.component.ts
+++ src/app/componentesEmpleado/responder-consultas/responder-consultas.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 
+
+// GRIS
+import {MatGridListModule} from '@angular/material/grid-list';
+
+// TABLAS
+import {MatTableModule} from '@angular/material/table';
+import { DatabaseService } from 'src/app/servicios/database.service';
+
+
+
 @Component({
   selector: 'app-responder-consultas',
   templateUrl: './responder-consultas.component.html',
@@ -10,8 +20,9 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class ResponderConsultasComponent implements OnInit {
 
   listadoConsultasSinResponder: any[] = [];
+  displayedColumns: string[] = ['nombre', 'apellido', 'fecha', 'correo', 'consulta', 'accion'];
 
-  constructor(private authService: AuthService, private firestore: AngularFirestore) { }
+  constructor(private authService: AuthService, private firestore: AngularFirestore, private dataBase : DatabaseService) { }
 
   ngOnInit(): void {
 
@@ -38,10 +49,32 @@ export class ResponderConsultasComponent implements OnInit {
 
 
   // esta funcion cambia el estado de la consulta a "respondida"
-  marcarComoResuelta()
+  marcarComoResuelta(correo, horaConsulta)
   {
+    let auxConsulta = null;
+    
+    this.firestore.collection("consultasAnonimas").get().subscribe((querySnapShot) => {
+      querySnapShot.forEach((doc) => {
+        if (doc.data()['correo'] == correo && doc.data()['horaConsulta'] == horaConsulta) 
+        {
+          console.log(doc.data());
+          auxConsulta = doc.data();
+          auxConsulta.estadoConsulta = "respondido";
+          this.dataBase.actualizar("consultasAnonimas", auxConsulta, doc.id);
+
+
+          setTimeout(() => {
+            auxConsulta = null; // seteo a null nuevamente la consulta.
+            this.ngOnInit(); // Esto hace que me vuelva a cargar el listado, con los estados cambiados.
+          }, 500);
+        }
+
+      })
+    })
 
   }
+
+
 
 
 
