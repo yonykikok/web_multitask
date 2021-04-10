d3a5d551fca6fa55df1e61ca37bddd47bb75d52e
yonykikok <jonathan.n.haedo@gmail.com>
2021-04-10 01:07:38 -0300

    ya nose que commit ponerle estoy limado, guardo cambios

--- src/app/componentes/detallado-publicacion/detallado-publicacion.component.ts
+++ src/app/componentes/detallado-publicacion/detallado-publicacion.component.ts
@@ -8,9 +8,11 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class DetalladoPublicacionComponent implements OnInit {
 
   publicacion;
+  esVistaCompleta;
   constructor( private dialogRef: MatDialogRef<DetalladoPublicacionComponent>,
     @Inject(MAT_DIALOG_DATA) data) { 
       this.publicacion = data.publicacion;
+      this.esVistaCompleta=data.esVistaCompleta;
     }
 
   ngOnInit(): void {
--- src/app/componentes/product-to-sell-card/product-to-sell-card.component.ts
+++ src/app/componentes/product-to-sell-card/product-to-sell-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { DetalladoPublicacionComponent } from '../detallado-publicacion/detallado-publicacion.component';
 
 @Component({
   selector: 'app-product-to-sell-card',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductToSellCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() publicacion;
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  openDialog(publicacion) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data={
+      publicacion:publicacion,
+      esVistaCompleta:true
+    }
+    const dialogRef = this.dialog.open(DetalladoPublicacionComponent,dialogConfig);
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
