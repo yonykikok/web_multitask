d875fc660f2bdbaf538fc79e6845fec361a1b6d9
Selene <seleneabraldez@gmail.com>
2021-04-13 22:03:30 -0300

    Agregado login en storywrap

--- src/app/componentes/story-wrap/story-wrap.component.ts
+++ src/app/componentes/story-wrap/story-wrap.component.ts
@@ -15,6 +15,14 @@ export class StoryWrapComponent implements OnInit {
 
 
   enviarEventoDeIngreso(){
+    let scrollToTop = window.setInterval(() => {
+      let pos = window.pageYOffset;
+      if (pos > 0) {
+          window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+          window.clearInterval(scrollToTop);
+      }
+  }, 16);
     this.ingresarEventClick.emit();
   }
 }
