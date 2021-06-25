05f25348581c7af56a22c0ba4e93acba41df5858
Lucho <lucho.moreno@live.com>
2021-06-25 17:26:45 -0300

    Modificar datos (Parte visual) - Falta responsive + funcionalidad

--- src/app/paginas/usuario/usuario/usuario.component.ts
+++ src/app/paginas/usuario/usuario/usuario.component.ts
@@ -3,6 +3,10 @@ import { Observable } from 'rxjs';
 import { Usuario } from 'src/app/clases/usuario';
 import { AuthService } from 'src/app/servicios/auth.service';
 
+//FORMBUILDER.
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
@@ -10,12 +14,24 @@ import { AuthService } from 'src/app/servicios/auth.service';
 })
 
 export class UsuarioComponent implements OnInit {
+
   selectPublicaciones = "activas";
   user: Usuario;
   currentUser$: Observable<Usuario>;
   pedido = "";
   mostrarDivNotificaciones = false;
   notificacioens = ["compro tal cosa", "vendio tal cosa", "permuto tal cosa"];
+
+
+  // VARIABLES PARA MODIFICAR DATOS CON FORM BUILDER. 
+
+  nuevosDatosForm: FormGroup;
+
+  nuevosDatosJSON = {
+    nombre: "",
+    apellido: "",
+  };
+
 
   //ADMIN
   boolTransacciones = false;
@@ -41,8 +57,66 @@ export class UsuarioComponent implements OnInit {
 
   // Todos
   boolNotificaciones = false;
+  mostrarDivEditar = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private formBuilder: FormBuilder) { 
+
+    this.nuevosDatosForm = this.formBuilder.group({
+
+      nombreValidado: ['', [Validators.required, Validators.minLength(4), Validators.pattern('^[a-zA-Z]{4,20}$')]],
+
+      apellidoValidado: ['', [Validators.required, Validators.minLength(4), Validators.pattern('^[a-zA-Z]{4,20}$')]],
+
+    });
+
+  }
+
+  public mostrarErrorRegistro(control: string): string {
+
+    let retorno = '';
+
+    switch (control) {
+
+      case 'nombreValidado':
+
+        if (this.nuevosDatosForm.controls.nombreValidado.hasError('required')) {
+          retorno = 'Debe ingresar un nombre.';
+        }
+
+        if (this.nuevosDatosForm.controls.nombreValidado.hasError('minLength')) {
+          retorno = 'Debe ingresar un nombre válido.';
+        }
+
+        if (this.nuevosDatosForm.controls.nombreValidado.hasError('pattern')) {
+          retorno = 'Error con el formato del nombre.';
+        }
+
+        break;
+
+      case 'apellidoValidado':
+
+        if (this.nuevosDatosForm.controls.apellidoValidado.hasError('required')) {
+          retorno = 'Debe ingresar un apellido.';
+        }
+
+        if (this.nuevosDatosForm.controls.apellidoValidado.hasError('minLength')) {
+          retorno = 'Debe ingresar un apellido válido.';
+        }
+
+        if (this.nuevosDatosForm.controls.apellidoValidado.hasError('pattern')) {
+          retorno = 'Error con el formato del apellido.';
+        }
+
+        break;
+
+    }
+
+    return retorno;
+  }
+
+
+
+
 
   ngOnInit(): void {
     this.currentUser$ = this.authService.obtenerUsuario$();
@@ -100,6 +174,10 @@ export class UsuarioComponent implements OnInit {
         behavior: "smooth"
       });
     }
+  }
+
+  actualizarDatos(){
+    
   }
 
 }
