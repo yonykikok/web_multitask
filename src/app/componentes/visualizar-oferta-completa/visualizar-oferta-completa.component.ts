06b5602e82bca20cf59ac0dc4088d7da3cb0243c
Lucho <lucho.moreno@live.com>
2021-07-06 00:02:04 -0300

    ROTO

--- src/app/componentes/visualizar-oferta-completa/visualizar-oferta-completa.component.ts
+++ src/app/componentes/visualizar-oferta-completa/visualizar-oferta-completa.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit, Input} from '@angular/core';
+import { Component, Inject, OnInit, Input } from '@angular/core';
 import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { DatabaseService } from 'src/app/servicios/database.service';
@@ -12,106 +12,123 @@ import { DetalladoPublicacionComponent } from '../detallado-publicacion/detallad
   styleUrls: ['./visualizar-oferta-completa.component.css']
 })
 export class VisualizarOfertaCompletaComponent implements OnInit {
-  
+
   publicacionOriginal;
 
 
   @Input() listaDeOfertas;
-  ampliarOferta=false;
+  ampliarOferta = false;
   constructor(public dialog: MatDialog, private dialogRef: MatDialogRef<VisualizarOfertaCompletaComponent>,
-    @Inject(MAT_DIALOG_DATA) data, private authService:AuthService, private dataBase: DatabaseService, private genNotificacion : GeneradorNotificacionesService) { 
-      this.listaDeOfertas = data.listaDeOfertas;
-      this.publicacionOriginal = data.publicacionOriginal;
-    }
-    
+    @Inject(MAT_DIALOG_DATA) data, private authService: AuthService, private dataBase: DatabaseService, private genNotificacion: GeneradorNotificacionesService) {
+    this.listaDeOfertas = data.listaDeOfertas;
+    this.publicacionOriginal = data.publicacionOriginal;
+  }
+
   ngOnInit(): void {
     // console.log(this.listaDeOfertas);
   }
-  generarObjetoPermuta(publicacion){
-      let permuta={
-        estadoPermuta:'pendienteDeConcretacion',
-        publicacionOfertada:publicacion,
-        idUserQuePublico:publicacion.idUserQuePublico,
-      };
-     publicacion.listaDeOfertas.forEach(oferta => {
-      if(oferta['estadoOferta']=="aceptadaParaPermutar"){
-        permuta['ofertaAceptada']=oferta;
-        permuta['idUserQueOferto']=oferta['idUserQueOferto'];
+  generarObjetoPermuta(publicacion) {
+    let permuta = {
+      estadoPermuta: 'pendienteDeConcretacion',
+      publicacionOfertada: publicacion,
+      idUserQuePublico: publicacion.idUserQuePublico,
+    };
+    publicacion.listaDeOfertas.forEach(oferta => {
+      if (oferta['estadoOferta'] == "aceptadaParaPermutar") {
+        permuta['ofertaAceptada'] = oferta;
+        permuta['idUserQueOferto'] = oferta['idUserQueOferto'];
         return;
       }
-     });  
-     
-     return permuta;
+    });
+
+    return permuta;
   }
 
 
 
-  aceptarOferta(oferta){
+  aceptarOferta(oferta) {
     this.publicacionOriginal.listaDeOfertas.forEach(auxOferta => {
 
-      if(auxOferta===oferta){
-          auxOferta.estadoOferta = 'aceptadaParaPermutar';
-            this.publicacionOriginal.estadoPublicacion="permutaPendiente";
-            this.dataBase.crear('permutas',this.generarObjetoPermuta(this.publicacionOriginal)).then((res)=>{
-              auxOferta['id']=res.id;
-              this.dataBase.actualizar('publicaciones', this.publicacionOriginal, this.publicacionOriginal.id).then(()=>{
-                alert("Oferta actualizada con exito");
-              }).catch(()=>{
-                alert("NO SE PUDO ENVIAR LA OFERTA!");
-              })  
-          });           
-        }
-      });     
-   }
-
-
-
-
-  aceptarORechazarOferta(oferta,estado)  
-  {  
-    this.publicacionOriginal.listaDeOfertas.forEach(auxOferta => {
-
-    if(auxOferta===oferta){
-        auxOferta.estadoOferta = estado;
-        if(estado==='aceptadaParaPermutar'){
-          
-            this.publicacionOriginal.estadoPublicacion="permutaPendiente";
-            this.dataBase.crear('permutas',this.generarObjetoPermuta(this.publicacionOriginal)).then((res)=>{
-            
-            auxOferta['id']=res.id;
-            this.dataBase.actualizar('publicaciones', this.publicacionOriginal, this.publicacionOriginal.id).then(()=>{
-
-            // NOTIFICACIÓN:
-
-            console.log(auxOferta.idUserQueOferto);
-            console.log(this.authService.user['id']);
-
-
-            this.genNotificacion.crearNotificacionCompraVenta(auxOferta.idUserQueOferto, this.authService.user['id'], "compraventa", "El usuario " + this.authService.user.nombre + " te ha aceptado tu permuta.");
-        
-            this.genNotificacion.crearNotificacionCompraVenta(this.authService.user['id'], auxOferta.idUserQueOferto, "compraventa", "Permuta ACEPTADA con éxito. Recuerden comunicarse mediante nuestro chat.");
-    
+      if (auxOferta === oferta) {
+        auxOferta.estadoOferta = 'aceptadaParaPermutar';
+        this.publicacionOriginal.estadoPublicacion = "permutaPendiente";
+        this.dataBase.crear('permutas', this.generarObjetoPermuta(this.publicacionOriginal)).then((res) => {
+          auxOferta['id'] = res.id;
+          this.dataBase.actualizar('publicaciones', this.publicacionOriginal, this.publicacionOriginal.id).then(() => {
             alert("Oferta actualizada con exito");
-
-             }).catch(()=>{
-                alert("NO SE PUDO ENVIAR LA OFERTA!");
-              })  
-
-           }); 
-        }
+          }).catch(() => {
+            alert("NO SE PUDO ENVIAR LA OFERTA!");
+          })
+        });
       }
-    });     
-     
+    });
+  }
+
+
+
+
+  aceptarORechazarOferta(oferta, estado) {
+    this.publicacionOriginal.listaDeOfertas.forEach(auxOferta => {
+
+      if (auxOferta === oferta) {
+        auxOferta.estadoOferta = estado;
+        if (estado === 'aceptadaParaPermutar') {
+
+          this.publicacionOriginal.estadoPublicacion = "permutaPendiente";
+          this.dataBase.crear('permutas', this.generarObjetoPermuta(this.publicacionOriginal)).then((res) => {
+
+            auxOferta['id'] = res.id;
+            this.dataBase.actualizar('publicaciones', this.publicacionOriginal, this.publicacionOriginal.id).then(() => {
+
+              this.genNotificacion.crearNotificacionCompraVenta(auxOferta.idUserQueOferto, this.authService.user['id'], "compraventa", "El usuario " + this.authService.user.nombre + " te ha aceptado tu permuta.");
+
+              this.genNotificacion.crearNotificacionCompraVenta(this.authService.user['id'], auxOferta.idUserQueOferto, "compraventa", "Permuta ACEPTADA con éxito. Recuerden comunicarse mediante nuestro chat.");
+
+              alert("Oferta aceptada con exito");
+
+            }).catch(() => {
+              alert("NO SE PUDO ENVIAR LA OFERTA!");
+            })
+
+          });
+        }
+
+        else {
+
+        
+
+          this.dataBase.eliminar('publicaciones', auxOferta.id).then(() => {
+
+           this.dataBase.actualizar('publicaciones', this.publicacionOriginal, this.publicacionOriginal.id).then(() => {
+
+            this.genNotificacion.crearNotificacionCompraVenta(auxOferta.idUserQueOferto, this.authService.user['id'], "compraventa", "El usuario " + this.authService.user.nombre + " te ha rechazado tu permuta.");
+
+            this.genNotificacion.crearNotificacionCompraVenta(this.authService.user['id'], auxOferta.idUserQueOferto, "compraventa", "Permuta RECHAZADA con éxito. Esperamos que las proximas ofertas sean mejores.");
+
+            alert("Oferta rechazada con exito");
+
+           }).catch(() => {
+             alert("NO SE PUDO ENVIAR LA OFERTA!");
+           })
+
+          });
+
+      
+        }
+
+      }
+    });
+
   }
 
 
   openDialog(publicacion) {
     const dialogConfig = new MatDialogConfig();
-    dialogConfig.data={
-      publicacion:publicacion,
-      esSoloVista:true
+    dialogConfig.data = {
+      publicacion: publicacion,
+      esSoloVista: true
     }
-    const dialogRef = this.dialog.open(DetalladoPublicacionComponent,dialogConfig);
+    const dialogRef = this.dialog.open(DetalladoPublicacionComponent, dialogConfig);
 
     dialogRef.afterClosed().subscribe(result => {
     });
