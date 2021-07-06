ba4cde0cb3247b4e12e6ed10e410ab55c6059195
Lucho <lucho.moreno@live.com>
2021-07-05 21:42:46 -0300

    Notificacion de permuta

--- src/app/componentes/seleccionar-mis-articulos/seleccionar-mis-articulos.component.ts
+++ src/app/componentes/seleccionar-mis-articulos/seleccionar-mis-articulos.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/servicios/auth.service';
 import { DatabaseService } from 'src/app/servicios/database.service';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { GeneradorNotificacionesService } from 'src/app/servicios/generador-notificaciones.service';
 @Component({
   selector: 'app-seleccionar-mis-articulos',
   templateUrl: './seleccionar-mis-articulos.component.html',
@@ -52,6 +53,7 @@ export class SeleccionarMisArticulosComponent implements OnInit {
   constructor(
     private dataBase: DatabaseService,
     private authService:AuthService,
+    private genNotificacion : GeneradorNotificacionesService
     // private dialogRef: MatDialogRef<SeleccionarMisArticulosComponent>,
     // @Inject(MAT_DIALOG_DATA) data
     
@@ -83,6 +85,14 @@ export class SeleccionarMisArticulosComponent implements OnInit {
           this.publicacionObjetivo.listaDeOfertas=listaOfertasExistentes;
           this.dataBase.actualizar('publicaciones',this.publicacionObjetivo,this.publicacionObjetivo.id).then(()=>{
             alert("Oferta enviada con exito");
+
+
+            // NOTIFICACIÓN:
+            this.genNotificacion.crearNotificacionCompraVenta(this.publicacionObjetivo.idUserQuePublico, this.authService.user['id'], "compraventa", "El usuario " + this.authService.user.nombre + " te ha ofrecido una permuta.");
+            // le avisa al que compró.                 
+            this.genNotificacion.crearNotificacionCompraVenta(this.authService.user['id'], this.publicacionObjetivo.idUserQuePublico, "compraventa", "Permuta realizada con éxito");
+    
+
           }).catch(()=>{
             alert("NO SE PUDO ENVIAR LA OFERTA!");
           })
